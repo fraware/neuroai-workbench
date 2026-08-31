50ab53b178cbc9a7bd5efeb3e9c35b2568d67a32
fraware <mateopetel@gmail.com>
2026-08-31 14:09:21 -0700

    feat: export discovery package surface for programmes
    Wire discovery exports so programmes, projection, and ClinicalTrials helpers share one entrypoint.

--- src/neuroai_workbench/discovery/__init__.py
+++ src/neuroai_workbench/discovery/__init__.py
@@ -5,6 +5,7 @@ never mutates a live monitor registry without human acceptance and an append-onl
 """
 
 from .boundary import DISCOVERY_BOUNDARY, DISCOVERY_NETWORK_ENV, DISCOVERY_SSRF_POLICY
+from .clinicaltrials import project_search_pages as project_clinicaltrials_search_pages
 from .errors import (
     DiscoveryAdjudicationRequiredError,
     DiscoveryError,
@@ -12,6 +13,19 @@ from .errors import (
     DiscoveryOverwriteRefusedError,
 )
 from .network import network_discovery_allowed, require_network_discovery_allowed, validate_discovery_url
+from .programme import (
+    OFFLINE_EXECUTABLE_UNIVERSE_IDS,
+    SU_TRIAL_DOC_ALIAS,
+    SU_TRIAL_ID,
+    list_programme_ids,
+    load_first_wave_programmes,
+    load_programme,
+    load_su_trial_programme,
+    programme_maturity,
+    run_source_universe,
+    validate_programme,
+)
+from .service import DiscoveryService
 from .store import (
     EXPECTED_FIXTURE_QUERY_IDS,
     get_fixture_query,
@@ -26,6 +40,7 @@ from .store import (
     seed_fixture_queries,
     store_query,
 )
+from .universe_projection import project_universe_pages
 from .workflow import (
     adjudicate_candidate_source,
     execute_discovery_query,
@@ -38,26 +53,39 @@ __all__ = [
     "DISCOVERY_NETWORK_ENV",
     "DISCOVERY_SSRF_POLICY",
     "EXPECTED_FIXTURE_QUERY_IDS",
+    "OFFLINE_EXECUTABLE_UNIVERSE_IDS",
+    "SU_TRIAL_DOC_ALIAS",
+    "SU_TRIAL_ID",
     "DiscoveryAdjudicationRequiredError",
     "DiscoveryError",
     "DiscoveryNetworkBlockedError",
     "DiscoveryOverwriteRefusedError",
+    "DiscoveryService",
     "adjudicate_candidate_source",
     "execute_discovery_query",
     "get_fixture_query",
     "get_offline_result_set",
     "initialize_discovery_workspace",
     "list_fixture_queries",
+    "list_programme_ids",
     "load_adjudication",
+    "load_first_wave_programmes",
     "load_proposal",
+    "load_programme",
     "load_query",
     "load_run",
+    "load_su_trial_programme",
     "load_successor",
     "network_discovery_allowed",
+    "programme_maturity",
+    "project_clinicaltrials_search_pages",
+    "project_universe_pages",
     "refuse_registry_overwrite",
     "require_accepted_proposals_for_successor",
     "require_network_discovery_allowed",
+    "run_source_universe",
     "seed_fixture_queries",
     "store_query",
     "validate_discovery_url",
+    "validate_programme",
 ]
